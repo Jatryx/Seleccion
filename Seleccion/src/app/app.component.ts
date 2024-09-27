5f11cb5ca4090e7538efedd17801858aaacf5536
MacarenaRJ <macarenarj96@gmail.com>
2024-09-27 08:15:54 +0200

    Formulario

--- Seleccion/src/app/app.component.ts
+++ Seleccion/src/app/app.component.ts
@@ -1,4 +1,3 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
--- Seleccion/src/app/candidatos-ofertados/candidatos-ofertados.component.ts
+++ Seleccion/src/app/candidatos-ofertados/candidatos-ofertados.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { OfertasService } from '../services/serviceOfertas/ofertas.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Ofertas } from '../models/modelOfertas/ofertas.model';
-import { MatButtonModule} from '@angular/material/button'
-import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
@@ -16,73 +16,104 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-
 @Component({
   selector: 'app-candidatos-ofertados',
   templateUrl: './candidatos-ofertados.component.html',
   styleUrls: ['./candidatos-ofertados.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, HttpClientModule, 
-    MatButtonModule, MatTableModule,
+  imports: [
+    ReactiveFormsModule, 
+    CommonModule, 
+    HttpClientModule, 
+    MatButtonModule, 
+    MatTableModule,
     MatPaginatorModule,
-    MatSortModule,MatToolbarModule, MatFormFieldModule,
+    MatSortModule,
+    MatToolbarModule, 
+    MatFormFieldModule,
     MatInputModule,
-    MatSelectModule],
+    MatSelectModule
+  ],
   providers: [OfertasService, UbicacionService]
-
 })
-export class CandidatosOfertadosComponent {
+export class CandidatosOfertadosComponent implements OnInit {
+  candidatoForm: FormGroup;
+  ofertaLista = new MatTableDataSource<Ofertas>([]);
+  provinciaLista: Ubicacion[];
 
-  constructor(private fb: FormBuilder, private ofertaService: OfertasService, private ubicacionService: UbicacionService,private dialog: MatDialog) {
+  displayedColumns: string[] = [
+    'candidato', 
+    'codope', 
+    'idPeticion', 
+    'proyecto', 
+    'cliente', 
+    'ubicacion', 
+    'perfil', 
+    'tecnologia', 
+    'estado', 
+    'fechaActualizacion', 
+    'resumen'
+  ];
+
+  constructor(
+    private fb: FormBuilder, 
+    private ofertaService: OfertasService, 
+    private ubicacionService: UbicacionService,
+    private dialog: MatDialog
+  ) {
+    this.candidatoForm = this.fb.group({
+      candidato: ['', Validators.required],
+      codope: ['', Validators.required],
+      proyecto: ['', Validators.required],
+      cliente: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      perfil: ['', Validators.required],
+      tecnologia: ['', Validators.required],
+      experiencia: ['', [Validators.required, Validators.min(0)]],
+      salario: ['', [Validators.required, Validators.min(0)]],
+      estado: ['', Validators.required],
+      fechaActualizacion: ['', Validators.required],
+      resumen: ['']
+    });
   }
 
-  ofertaLista:Ofertas[]
-  provinciaLista:Ubicacion[]
-
-  candidatoForm = this.fb.group({
-    candidato: ['', Validators.required],
-    proyecto: ['', Validators.required],
-    cliente: ['', Validators.required],
-    ubicacion: ['', Validators.required],
-    perfil: ['', Validators.required],
-    tecnologia: ['', Validators.required],
-    experiencia: ['', [Validators.required, Validators.min(0)]],
-    salario: ['', [Validators.required, Validators.min(0)]],
-    estado: ['', Validators.required],
-    fechaActualizacion: ['', Validators.required],
-    resumen: ['']
-  });
-
-  ngOnInit(){
-      this.cargarOfertas();
-      this.cargarProvincias()
+  ngOnInit() {
+    this.cargarOfertas();
+    this.cargarProvincias();
   }
-  
-  cargarOfertas(){
-    this.ofertaService.getOfertas().subscribe(data => {
-      this.ofertaLista = data;
-    },
-    error => {
-      console.error('Error al cargar las ofertas:', error);
-    })
+
+  cargarOfertas() {
+    this.ofertaService.getOfertas().subscribe(
+      data => {
+        this.ofertaLista.data = data;
+      },
+      error => {
+        console.error('Error al cargar las ofertas:', error);
+      }
+    );
   }
-  cargarProvincias(){
-    this.ubicacionService.getUbicaciones().subscribe(data => {
-      this.provinciaLista = data;
-    },
-    error => {
-      console.error('Error al cargar las ofertas:', error);
-    })
+
+  cargarProvincias() {
+    this.ubicacionService.getUbicaciones().subscribe(
+      data => {
+        this.provinciaLista = data;
+      },
+      error => {
+        console.error('Error al cargar las provincias:', error);
+      }
+    );
   }
 
   onSubmit() {
     if (this.candidatoForm.valid) {
-      console.log(this.candidatoForm.value);
+      const nuevoCandidato = this.candidatoForm.value;
+      this.ofertaLista.data = [...this.ofertaLista.data, nuevoCandidato];
+      this.candidatoForm.reset();
     } else {
       console.log('Formulario no válido');
     }
   }
-  // Método para abrir el popup directamente en el componente
+
   openPopup(prueba: Ofertas): void {
     const dialogRef = this.dialog.open(DialogContent, {
       width: '400px',
@@ -118,7 +149,6 @@ export class CandidatosOfertadosComponent {
     Cerrar
   </button>
 </div>
-
   `,
 })
 export class DialogContent {
@@ -128,4 +158,3 @@ export class DialogContent {
     this.dialogRef.close();
   }
 }
-
